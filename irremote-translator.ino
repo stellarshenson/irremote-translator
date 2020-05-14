d24923257762779035317152784eeebecbd829e6
stellarshenson <konrad.jelen@gmail.com>
2020-05-14 12:27:39 +0200

    reduced memory footprint

--- irremote-translator.ino
+++ irremote-translator.ino
@@ -24,7 +24,22 @@
 #define TRIGGER_IRCODE_CANCELED 4
 
 #define DEBUG_LEVEL 2 //0 - debug off, 1 - essential messages, 2 - full diagnostics
-#define NUMCODES 16 //number of codes stored in memory
+
+#define IRCODES_NUMBER       16 //number of codes stored in memory
+#define IRCODES_SRC_CODETYPE 0
+#define IRCODES_SRC_CODELEN  1
+#define IRCODES_SRC_CODE0    2
+#define IRCODES_SRC_CODE1    3
+#define IRCODES_SRC_CODE2    4
+#define IRCODES_SRC_CODE3    5
+#define IRCODES_TGT_CODETYPE 6
+#define IRCODES_TGT_CODELEN  7
+#define IRCODES_TGT_CODE0    8
+#define IRCODES_TGT_CODE1    9
+#define IRCODES_TGT_CODE2    10
+#define IRCODES_TGT_CODE3    11
+
+
 
 //IR receiver setup
 IRrecv irrecv(RECV_PIN);
@@ -35,7 +50,7 @@ decode_results results;
 void sendCode(uint8_t repeat, uint8_t codeType, uint8_t codeLen, unsigned long codeValue);
 void storeCode(decode_results *results, int8_t &codeType, uint8_t &codeLen, unsigned long &codeValue);
 
-static uint8_t savedCodes[NUMCODES][12] = {0}; //static initialisation to 0 of all elements
+static uint8_t irCodes[IRCODES_NUMBER][12] = {0}; //static initialisation to 0 of all elements
 
 uint8_t codeType = 255; // The type of code, 255 is unsigned equivalent of -1
 uint8_t codeLen = 0; // The length of the code
@@ -75,25 +90,26 @@ void setup()
   uint8_t initStatus = EEPROM.read(0); //if initStatus=1 initialised, initStatus=0 uninitialised
   
   //system not initialised, writing zeros to eeprom
-  if(initStatus == 0) {
-    saveIRCodesToEEPROM();
-  } 
-  //system initialised, read all eeprom cells
-  else {
-    int c = sizeof(savedCodes[0])/sizeof(savedCodes[0][0]); //number of columns
-    uint8_t eepromAddr = 1; //start with 1st eeprom cell, number 0 is reserved for status
-    for(int i=0; i<NUMCODES; i++) for(int j=0; j<c; j++) savedCodes[i][j] = EEPROM.read(eepromAddr++);
-  }
+  if(initStatus == 0) saveIRCodesToEEPROM();
+  else getIRCodesFromEEPROM();
+
+  //find out if we have any codes recorded - and count them
+  for(int i=0; i<IRCODES_NUMBER; i++) irCodesAvailable = (irCodesAvailable > 0 || irCodes[i][IRCODES_SRC_CODELEN] > 0 ? ++irCodesAvailable : irCodesAvailable);
 
   //initialise state machine
   fsm.add_transition(&state_ircode_sense, &state_ircode_record, TRIGGER_IRCODE_RECORD, NULL);
   fsm.add_transition(&state_ircode_record, &state_ircode_sense, TRIGGER_IRCODE_RECORDED, NULL);
   fsm.add_transition(&state_ircode_record, &state_ircode_sense, TRIGGER_IRCODE_CANCELED, NULL);  
+
+  if(DEBUG_LEVEL == 2) { 
+    Serial.print(F("[INIT] System intiated with ")); 
+    Serial.print(irCodesAvailable, DEC); 
+    Serial.println(F(" recorded IR codes"));
+  }
 }
 
 void loop() {
   fsm.run_machine(); //run state loops
-  delay(10);
 }
 
 
@@ -107,7 +123,7 @@ void loop() {
 * only announces the transition
 */
 void on_ircode_sense_enter() {
-  if(DEBUG_LEVEL) Serial.println("[IRCODE SENSE] Start. Listening for ircode and ircode record button");  
+  if(DEBUG_LEVEL) Serial.println(F("[IRCODE SENSE] Start. Listening for ircode and ircode record button"));  
   
   //interrupt initialisation
   ircode_record_detected = 0;
@@ -125,7 +141,7 @@ void on_ircode_sense_loop() {
   // button to enable recording ircode
   // driven by interrupts now on IRCODE_RECORD_PIN
   if (ircode_record_detected == 1) {
-    if(DEBUG_LEVEL) Serial.println("[IRCODE SENSE] IRCode recording detected");
+    if(DEBUG_LEVEL) Serial.println(F("[IRCODE SENSE] IRCode recording detected"));
     blink(STATUS_LED_PIN, 3, 300);
     fsm.trigger(TRIGGER_IRCODE_RECORD);
   } 
@@ -135,7 +151,7 @@ void on_ircode_sense_loop() {
 * signals exit from the state
 */
 void on_ircode_sense_exit() {
-  if(DEBUG_LEVEL == 2) Serial.println("[IRCODE SENSE] Exit. Disabling audio signal detection");  
+  if(DEBUG_LEVEL == 2) Serial.println(F("[IRCODE SENSE] Exit. Disabling ir signal detection"));  
   disableInterrupt(IRCODE_RECORD_PIN);
 }
 
@@ -155,7 +171,7 @@ void on_ircode_record_enter() {
   
   digitalWrite(IRCODE_STORED_LED_PIN, LOW);
   digitalWrite(IRCODE_RECORD_LED_PIN, HIGH);
-  if(DEBUG_LEVEL) Serial.println("[IRCODE RECORD] Start.  Enabling IR receiver");
+  if(DEBUG_LEVEL) Serial.println(F("[IRCODE RECORD] Start.  Enabling IR receiver"));
 }
 
 /**
@@ -171,7 +187,7 @@ void on_ircode_record_loop() {
   if (irrecv.decode(&results)) {
     //record start audio code
     if( codeType == 255 ) {
-      if(DEBUG_LEVEL) Serial.println("[IRCODE RECORD][1/2] - SIGNAL ircode detected");
+      if(DEBUG_LEVEL) Serial.println(F("[IRCODE RECORD][1/2] - SIGNAL ircode detected"));
       blink(STATUS_LED_PIN, 3, 300);
       storeCode(&results, codeType, codeLen, codeValue);      
       irrecv.resume(); //resume recording for the stop code
@@ -186,7 +202,7 @@ void on_ircode_record_loop() {
 * announces exit from ircode recording state and turns off recording LED
 */
 void on_ircode_record_exit() {
-  if(DEBUG_LEVEL == 2) Serial.println("[IRCODE RECORD] Exit. Disabling receiver");
+  if(DEBUG_LEVEL == 2) Serial.println(F("[IRCODE RECORD] Exit. Disabling receiver"));
   digitalWrite(IRCODE_RECORD_LED_PIN, LOW);
   digitalWrite(IRCODE_STORED_LED_PIN, HIGH);
 }
@@ -300,8 +316,18 @@ static void  blink( const byte pin, const byte cycles, const unsigned int durati
  * this function saves the ircodes to eeprom
  */
 void saveIRCodesToEEPROM() {
-    int c = sizeof(savedCodes[0])/sizeof(savedCodes[0][0]); //number of columns
+    int c = sizeof(irCodes[0])/sizeof(irCodes[0][0]); //number of columns
     uint8_t eepromAddr = 1; //start with 1st eeprom cell, number 0 is reserved for status
-    for(int i=0; i<NUMCODES; i++) for(int j=0; j<c; j++) EEPROM.update(eepromAddr++, savedCodes[i][j]);
+    for(int i=0; i<IRCODES_NUMBER; i++) for(int j=0; j<c; j++) EEPROM.update(eepromAddr++, irCodes[i][j]);
     EEPROM.update(0, 1); //set initialised status  
+}
+
+
+/**
+ * this function gets the codes from eeprom
+ */
+void getIRCodesFromEEPROM() {
+    int c = sizeof(irCodes[0])/sizeof(irCodes[0][0]); //number of columns
+    uint8_t eepromAddr = 1; //start with 1st eeprom cell, number 0 is reserved for status
+    for(int i=0; i<IRCODES_NUMBER; i++) for(int j=0; j<c; j++) irCodes[i][j] = EEPROM.read(eepromAddr++);
 }
